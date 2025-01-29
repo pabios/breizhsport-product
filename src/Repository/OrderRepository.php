ec7709638d5b6b2c760e8d9729c7c4cb2b9242f7
pabios <pabios.pabiosoft@gmail.com>
2025-01-29 19:28:44 +0100

    Feat: add admin board

--- src/Repository/OrderRepository.php
+++ src/Repository/OrderRepository.php
@@ -16,6 +16,50 @@ class OrderRepository extends ServiceEntityRepository
         parent::__construct($registry, Order::class);
     }
 
+    public function countOrdersByMonth(): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = "
+        SELECT TO_CHAR(order_date, 'YYYY-MM') AS month, COUNT(id) AS count
+        FROM orders
+        GROUP BY month
+        ORDER BY month ASC
+    ";
+
+        $stmt = $conn->prepare($sql);
+        $resultSet = $stmt->executeQuery();
+
+        return $resultSet->fetchAllAssociative();
+    }
+
+
+    public function countOrdersByMonthOldFeat(): array
+    {
+        return $this->createQueryBuilder('o')
+            ->select("TO_CHAR(o.orderDate, 'YYYY-MM') as month, COUNT(o.id) as count")
+            ->groupBy("month")
+            ->orderBy("month", 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+
+
+
+
+
+    // src/Repository/OrderRepository.php
+    public function getTotalRevenue(): float
+    {
+        return (float) $this->createQueryBuilder('o')
+            ->select('SUM(o.totalAmount)')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+
+
     //    /**
     //     * @return Order[] Returns an array of Order objects
     //     */
